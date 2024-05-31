9f22e10ce52f2178a4f61fe222368a68da60f8e3
abobakaraslam <164588250+abobakaraslam@users.noreply.github.com>
2024-05-31 12:46:38 +0500

    Register.jsx

--- src/component/Register.jsx
+++ src/component/Register.jsx
@@ -98,7 +98,7 @@ function Register() {
                         Confirm password
                       </label>
                       <input
-                        type="confirm-password"
+                        type="password"
                         name="confirm-password"
                         id="confirm-password"
                         placeholder="••••••••"
